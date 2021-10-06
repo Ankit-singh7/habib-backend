6e8dc5aea74aa0a17fb5fbe6c3e8c2e7925c263d
Ankit-singh7 <ankit.as2307@gmail.com>
2021-10-07 02:06:14 +0530

    update

--- controllers/productSalesReportController.js
+++ controllers/productSalesReportController.js
@@ -13,10 +13,13 @@ const salesReportModel = mongoose.model('productSalesReport');
 
 let getAllSalesReport = (req, res) => {
     const filters = req.query;
+    const startDate = req.query.startDate
+    const endDate = req.query.endDate
+    delete filters.startDate
+    delete filters.endDate
     console.log(filters)
-    if(Object.keys(filters).length) {
         console.log('object')
-        salesReportModel.find({'date':{ $gte:req.query.startDate, $lte:req.query.endDate}}).exec((err,result) => {
+        salesReportModel.find({'date':{ $gte:startDate, $lte:endDate}}).exec((err,result) => {
             if(err) {
                 res.send(err)
             } else if (check.isEmpty(result)) {
@@ -44,41 +47,7 @@ let getAllSalesReport = (req, res) => {
 
             }
         })
-    } else {
-     console.log('here')
-          salesReportModel.find()
-            .lean()
-            .select('-__v -_id')
-            .exec((err, result) => {
-                if (err) {
-                    console.log(err)
-                    logger.error(err.message, 'Sales Report Controller: getAllSalesReport', 10)
-                    let apiResponse = response.generate(true, 'Failed To Find ', 500, null)
-                    res.send(apiResponse)
-                } else if (check.isEmpty(result)) {
-                    logger.info('No Data Found', 'Ingredient Controller: getAllIngredient')
-                    let apiResponse = response.generate(true, 'No Data Found', 404, null)
-                    res.send(apiResponse)
-                } else {
-                    const filteredUsers = result.filter(user => {
-                        console.log('here', user)
-                        let isValid = true;
-                        for (key in filters) {
-                            console.log(filters[key])
-                            console.log('here', user[key])
-                                isValid = isValid && user[key] == filters[key];    
-                        }
-                        return isValid;
-                    });
-                    let total = filteredUsers.length;
-                    let reportList = filteredUsers
-                    let newResult = {total:total,result:reportList}
-                    let apiResponse = response.generate(false, 'All Bills Found', 200, newResult)
-                    res.send(apiResponse)
-                }
-            })
-    }
-}
+    } 
 
 
 
