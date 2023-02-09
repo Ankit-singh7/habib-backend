0e53f1c30e71c1fe1d80b8ba25719ddeb8566071
Ankit-singh7 <ankit.as2307@gmail.com>
2023-02-10 01:12:28 +0530

    closing_balance

--- models/SessionModel.js
+++ models/SessionModel.js
@@ -45,6 +45,10 @@ const sessionSchema = new Schema({
     type:Number,
     default: 0
   },
+  closing_balance: {
+    type:Number,
+    default: 0
+  },
   date:{
     type:String,
     default:''
